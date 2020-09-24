41732f6c5ccc92c7df61015f88eddf6be2a53421
Insan Al-Amin <musiklib@gmail.com>
2020-09-24 12:37:11 +0700

    Create course-1-mysql-create-insert-table.sql

---
+++ basis-data-lanjut/2020-2021/course-1-mysql-create-insert-table.sql
@@ -0,0 +1,32 @@
+CREATE TABLE siswa (
+    id INT,
+    nama VARCHAR(255),
+    jenis_kelamin TINYINT,
+    alamat VARCHAR(255),
+    tanggal_lahir DATE,
+    id_elemen_peminatan TINYINT,
+    kode_wilayah VARCHAR(10)
+);
+
+INSERT INTO siswa VALUES
+(NULL, 'Wawan Junaedi', 1, 'Komplek Rancacelsea no. 33', '2006-03-13', 3, '3273'),
+(NULL, 'Karsimah', 2, 'Jalan Jamparing Kulon 88 B', '2007-08-11', 6, '3204'),
+(NULL, 'Windy Zeroblast', 2, 'Jalan Kautamaan Sedekah no. 13', '2007-09-15', 1, '3277'),
+(NULL, 'Kankan Sukamtar', 1, 'Blok Kayu Jati 28B/66 RW 01 RT 03', '2006-01-28', 4, '3277'),
+(NULL, 'Ginyawa', 1, 'Jalan Paseman Legok No. 29 Babakan Hilir', '2007-11-04', 1, '3273'),
+(NULL, 'Pande Rereogan', 1, 'Jalan Kulitiba No. 2A', '2008-04-09', 1, '3277'),
+(NULL, 'Wakun Carmadi', 1, 'Jalan Daringlila No 9', '2007-04-28', 4, '3217'),
+(NULL, 'Vincent Vinal Vantasi', 1, 'Jl. Kersamanah Kidul No 33', '2007-12-21', 6, '3211'),
+(NULL, 'Wildan Andi P.', 1, 'Blok B6 No. 17 Komplek Pancawasra', '2007-06-04', 5, '3204'),
+(NULL, 'Selena Zainulmutakin', 2, 'Komplek Perlantaskorda Blok J3 No. 29', '2008-03-17', 6, '3273'),
+(NULL, 'Binoronggo Saswajiman', 1, 'Jl. Santosa Seharila No. 77 Blok C3', '2007-07-18', 2, '3273'),
+(NULL, 'Dolan', 1, 'Jalan Serengeti Raya No. 33', '2007-04-25', 6, '3205'),
+(NULL, 'Priyanka Ercel', 2, 'Jalan Golf Awis Rupiah No. 68 C', '2006-10-23', 6, '3277'),
+(NULL, 'Urassaya Lambada', 2, 'Komplek Tegalwacan Blok D3 No 6', '2007-09-08', 3, '3204'),
+(NULL, '', _, '', '', _, '32'),
+(NULL, '', _, '', '', _, '32'),
+(NULL, '', _, '', '', _, '32'),
+(NULL, '', _, '', '', _, '32'),
+(NULL, '', _, '', '', _, '32'),
+(NULL, '', _, '', '', _, '32')
+;
