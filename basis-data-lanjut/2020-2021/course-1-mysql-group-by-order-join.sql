1730ca86ed9bdbfda7a91e65557b187e13e721d5
Insan Al-Amin <musiklib@gmail.com>
2020-09-24 13:54:28 +0700

    Update course-1-mysql-group-by-order-join.sql

--- basis-data-lanjut/2020-2021/course-1-mysql-group-by-order-join.sql
+++ basis-data-lanjut/2020-2021/course-1-mysql-group-by-order-join.sql
@@ -1,1 +1,8 @@
-
+SELECT  
+siswa.id_elemen_peminatan,
+elemen.nama AS nama_elemen_peminatan,
+COUNT(*) as jumlah
+FROM siswa
+JOIN elemen ON elemen.id = siswa.id_elemen_peminatan
+GROUP BY siswa.id_elemen_peminatan
+ORDER BY COUNT(*) DESC
